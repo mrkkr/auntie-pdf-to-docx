ff95c3d155bb0caf44254224be985774a84375d6
btahir89 <bilal.tahir89@gmail.com>
2025-03-07 19:07:44 -0800

    add ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@/components/analytics'
 import './globals.css'
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <Analytics />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-50`}
       >
